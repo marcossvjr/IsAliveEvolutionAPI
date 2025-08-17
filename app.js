12d43f3cb6db3115b7610ac171c05d1aaec56e11
Marcos Vieira <marcossvjr@hotmail.com>
2025-08-17 01:25:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const https = require('https');
 const http = require('http');
 
-const url = 'https://isaliveevolutionapi.onrender.com';//'https://evolution-api-v2-2-3-4vxf.onrender.com';
-const porta = 80;
+const url = 'https://isaliveevolutionapi.onrender.com:8080';//'https://evolution-api-v2-2-3-4vxf.onrender.com';
+const porta = 8080;
 let ultimaResposta = 'Ainda sem resposta da API externa.';
 let getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
+let timeoutId = null;
 // Função que faz a requisição GET
 function fazerRequisicao() {
+  if (timeoutId)
+    clearTimeout(timeoutId);
+  
   https.get(url, (res) => {
     let dados = '';
 
@@ -26,17 +29,17 @@ function fazerRequisicao() {
 
   let timeout = getRandomInteger(30000, 50000);
   console.log(`Próxima requisição em ${timeout/1000}s [Agora: ${new Date().toISOString()} | ${new Date((new Date().getTime() + timeout)).toISOString()}]`)
-  setTimeout(fazerRequisicao, timeout);
+  timeoutId = setTimeout(fazerRequisicao, timeout);
 }
 
 fazerRequisicao();
 
 // Cria o servidor HTTP local
-//const servidor = http.createServer((req, res) => {
-//  res.writeHead(200, { 'Content-Type': 'application/json' });
-//  res.end(ultimaResposta);
-//});
+const servidor = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(ultimaResposta);
+});
 
-//servidor.listen(porta, () => {
-//  console.log(`Servidor HTTP rodando em http://localhost:${porta}`);
-//});
+servidor.listen(porta, () => {
+  console.log(`Servidor HTTP rodando em http://localhost:${porta}`);
+});
